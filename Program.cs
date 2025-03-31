964e04c62f75cb3c757838d1f4ad2b77d2aa3cfa
ThiagoRibeiro16 <thiago.escola321@gmail.com>
2025-03-30 21:37:42 -0300

    finaliza programa

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+string medida1;
+double milha;
+double km;
+
+Console.Clear();
+
+Console.Write("Informe a medida em milhas: ");
+medida1 = Console.ReadLine()!;
+
+milha = Convert.ToDouble(medida1);
+km = milha * 1.609;
+
+Console.WriteLine($"medida em milhas: {milha}");
+Console.WriteLine($"medida em quilômetros: {km} km");
